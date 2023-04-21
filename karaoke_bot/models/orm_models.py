347d5fadb990be8cc0a63d4e6cfa4f518fd7ab1d
gra4evp <petrgrachev1@gmail.com>
2023-04-22 01:33:34 +0300

    add models

--- karaoke_bot/models/orm_models.py
+++ karaoke_bot/models/orm_models.py
@@ -12,10 +12,8 @@ class TelegramLogin:
     username: str
     language_code: str
     is_premium: bool
-    added_to_attachment_menu: bool
-    can_join_groups: bool
-    can_read_all_group_messages: bool
-    supports_inline_queries: bool
+    created_at: datetime
+    updated_at: datetime
 
 
 @dataclass
