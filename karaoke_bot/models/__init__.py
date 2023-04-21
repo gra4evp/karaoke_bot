1b30421120bb47f8eb889579f8c36bca374cdf9a
gra4evp <petrgrachev1@gmail.com>
2023-04-22 01:07:40 +0300

    add models

---
+++ karaoke_bot/models/__init__.py
@@ -0,0 +1,2 @@
+from .orm_models import TelegramLogin, Account, Visitor, Moderator, Owner, Administrator, VisitorPerformance, Session, \
+    Karaoke, TrackVersion, Track, Artist
---
+++ karaoke_bot/models/orm_models.py
@@ -0,0 +1,107 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class TelegramLogin:
+    account_id: int
+    id: int
+    is_bot: bool
+    first_name: str
+    last_name: str
+    username: str
+    language_code: str
+    is_premium: bool
+    added_to_attachment_menu: bool
+    can_join_groups: bool
+    can_read_all_group_messages: bool
+    supports_inline_queries: bool
+
+
+@dataclass
+class Account:
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class Visitor:
+    id: int
+    selected_karaoke_id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class Moderator:
+    id: int
+    karaoke_id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class Owner:
+    id: int
+    password: str
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class Administrator:
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class VisitorPerformance:
+    id: int
+    visitor_id: datetime
+    track_version_id: datetime
+    session_id: int
+
+
+@dataclass
+class Session:
+    id: int
+    karaoke_id: int
+    timestamp_from: datetime.now()
+    timestamp_to: datetime.now()
+
+
+@dataclass
+class Karaoke:
+    id: int
+    name: str
+    active: bool
+    owner_id: int
+    avatar_id: int
+    description: str
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class TrackVersion:
+    id: int
+    track_id: int
+    url: str
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class Track:
+    id: int
+    name: int
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class Artist:
+    id: int
+    name: int
