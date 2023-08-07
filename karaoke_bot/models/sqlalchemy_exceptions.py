10b3fc95cee6c3c3903404d06137ac524c9f5291
gra4evp <petrgrachev1@gmail.com>
2023-08-08 01:07:58 +0300

    add EmptyFieldError

--- karaoke_bot/models/sqlalchemy_exceptions.py
+++ karaoke_bot/models/sqlalchemy_exceptions.py
@@ -14,3 +14,9 @@ class InvalidAccountStateError(Exception):
     def __init__(self, account_id: int, state: str):
         message = f"Account with id={account_id} is in an invalid state: {state}."
         super().__init__(message)
+
+
+class EmptyFieldError(Exception):
+    def __init__(self, table_name: str, field_name: str):
+        message = f"{table_name} has an empty field '{field_name}'"
+        super().__init__(message)
