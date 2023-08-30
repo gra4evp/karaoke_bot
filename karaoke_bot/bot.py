cafbbe79596b283d5dfa1aa7bb449270bba69d5f
gra4evp <petrgrachev1@gmail.com>
2023-08-30 17:02:29 +0300

    ...

--- karaoke_bot/bot.py
+++ karaoke_bot/bot.py
@@ -6,7 +6,6 @@ from handlers import admin, other, visitor, owner, moderator
 other.register_other_handlers(dispatcher)
 owner.register_owner_handlers(dispatcher)
 visitor.register_visitor_handlers(dispatcher)
-
 admin.register_admin_handlers(dispatcher)
 
 executor.start_polling(dispatcher, skip_updates=True)
