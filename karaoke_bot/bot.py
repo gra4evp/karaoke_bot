e118f1846c85a30d3f6e2fdb2f4b5a9c382d37ee
gra4evp <petrgrachev1@gmail.com>
2023-09-12 02:53:24 +0300

    import fix

--- karaoke_bot/bot.py
+++ karaoke_bot/bot.py
@@ -1,13 +1,15 @@
 from aiogram.utils import executor
 from create_bot import dispatcher
-from karaoke_bot.handlers.scripts.common import other
-from karaoke_bot.handlers.scripts.visitor import visitor
-from karaoke_bot.handlers.scripts.admin import admin
-from karaoke_bot.handlers.scripts.owner import owner
+from karaoke_bot.handlers.scripts import admin, moderator, owner, visitor, common
 
-other.register_other_handlers(dispatcher)
-owner.register_owner_handlers(dispatcher)
-visitor.register_visitor_handlers(dispatcher)
-admin.register_admin_handlers(dispatcher)
+
+common.other.register_handlers(dispatcher)
+
+owner.new_karaoke.register_handlers(dispatcher)
+owner.queue.register_handlers(dispatcher)
+
+visitor.search_karaoke.register_handlers(dispatcher)
+visitor.order_track.register_handlers(dispatcher)
+visitor.change_selected_karaoke.register_handlers(dispatcher)
 
 executor.start_polling(dispatcher, skip_updates=True)
--- karaoke_bot/handlers/__init__.py
+++ karaoke_bot/handlers/__init__.py
@@ -1,5 +1,1 @@
-from .scripts.common import other
-from .scripts.visitor import visitor
-from .scripts.moderator import moderator
-from .scripts.admin import admin
-from .scripts.owner import owner
+from . import scripts
