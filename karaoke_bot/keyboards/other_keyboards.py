7b586c6c497217566d9953d43dff2a201b0a351f
gra4evp <petrgrachev1@gmail.com>
2023-10-28 20:23:14 +0300

    menu button

--- karaoke_bot/keyboards/other_keyboards.py
+++ karaoke_bot/keyboards/other_keyboards.py
@@ -4,9 +4,9 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 other_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 
 order_track = KeyboardButton("Order a track")
-status = KeyboardButton("Status")
+menu = KeyboardButton("Menu")
 cancel = KeyboardButton("Cancel")
 
 other_keyboard.add(order_track)
-other_keyboard.add(status)
+other_keyboard.add(menu)
 other_keyboard.insert(cancel)
