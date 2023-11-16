92f1290d8513f253dc657a74f488725a207233ed
gra4evp <petrgrachev1@gmail.com>
2023-11-16 20:21:20 +0300

    delete button search_karaoke

--- karaoke_bot/keyboards/other_keyboards.py
+++ karaoke_bot/keyboards/other_keyboards.py
@@ -3,12 +3,10 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 other_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 
-search_karaoke = KeyboardButton('Search karaoke 🔍')
 order_track = KeyboardButton("Order a track")
 menu = KeyboardButton("Menu")
 cancel = KeyboardButton("Cancel")
 
-other_keyboard.add(search_karaoke)
 other_keyboard.add(order_track)
 other_keyboard.add(menu)
 other_keyboard.insert(cancel)
