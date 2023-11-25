be3d72bf2f1233daf3890d68f3afe5823758011a
gra4evp <petrgrachev1@gmail.com>
2023-11-25 17:23:49 +0300

    add audio_processing.py

--- spectrogram/spectrogram_multithreading_v2.py
+++ spectrogram/spectrogram_multithreading_v2.py
@@ -3,40 +3,9 @@ import os
 import time
 import threading
 from queue import Queue
+from audio_processing import get_spectrograms
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-def get_audio_sample(audio, sr: float, start: float, duration: float):
-    return audio[start * sr:(start + duration) * sr]
-
-
-def get_spectrogram_sample(
-        audio,
-        sr: float,
-        start: float,
-        duration: float,
-        volume: float = 1,
-        frame_size: int = 2048,
-        hop_size: int = 512
-):
-
-    sample = get_audio_sample(audio, sr, start=start, duration=duration) * volume
-    return librosa.stft(sample, n_fft=frame_size, hop_length=hop_size)
-
-
-def get_spectrograms(audio, sr: float, n_samples: int, start: float, duration: float, overlap: float):
-    spectrograms = []
-    for i in range(n_samples):
-        spec = get_spectrogram_sample(
-            audio=audio,
-            sr=sr,
-            start=start + i*(duration - overlap),
-            duration=duration
-        )
-        spectrograms.append(spec)
-
-    return spectrograms
 
 
 def process_files(
