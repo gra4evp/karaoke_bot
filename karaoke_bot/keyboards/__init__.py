b55ab0068c2a49c157c7203a64e24360e2b3b450
gra4evp <petrgrachev1@gmail.com>
2023-12-12 02:17:38 +0300

    add loc_search_karaoke.py

--- karaoke_bot/keyboards/__init__.py
+++ karaoke_bot/keyboards/__init__.py
@@ -1,3 +1,2 @@
 from .client_keyboards import client_keyboard
 from .other_keyboards import other_keyboard
-from .owner.new_karaoke_keyboards import keyboards
---
+++ karaoke_bot/localization/local_files/scripts/visitor/loc_search_karaoke.py
@@ -0,0 +1,50 @@
+local_dict = {
+    'search_karaoke_command': {
+        'messages': {
+            'enter_name': {
+                'en': f"Enter the <b>NAME</b> of the virtual karaoke where you plan to perform.",
+                'ru': "Введите <b>ИМЯ</b> виртуального караоке, в котором вы планируете выступать."
+            }
+        }
+    },
+
+    'search_karaoke': {
+        'messages': {
+            'no_karaoke': {
+                'en': "Oops, there is no such karaoke yet.\n\n"
+                      "Try to get the <b>NAME</b> from the administrator of the institution where you are.",
+                'ru': "Упс, такого караоке пока нет.\n\n"
+                      "Попробуйте узнать <b>ИМЯ</b> у администратора заведения, в котором вы находитесь."
+            },
+
+            'karaoke': {
+                'en': "<b>Karaoke</b>:",
+                'ru': "<b>Караоке</b>: "
+            },
+
+            'owner': {
+                'en': "<b>Owner</b>:",
+                'ru': "<b>Владелец</b>:"
+            },
+
+            'subscribers': {
+                'en': "<b>Subscribers</b>:",
+                'ru': "<b>Подписчиков</b>:"
+            },
+
+            'already_sub': {
+                'en': "\n\n✅ You have already subscribed!",
+                'ru': "\n\n✅ Вы уже подписались!"
+            }
+        }
+    },
+
+    'callback_subscribe_to_karaoke': {
+        'messages': {
+            'you_sub': {
+                'en': "✅ Success! You have subscribed!",
+                'ru': "✅ Успех! Вы подписались!"
+            }
+        }
+    }
+}
