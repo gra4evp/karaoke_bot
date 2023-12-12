9cfe80df6dbd340266453cc2c5522456b95fe6e9
gra4evp <petrgrachev1@gmail.com>
2023-12-12 22:57:12 +0300

    refactoring callbacks_data

--- karaoke_bot/keyboards/keyboard_factory.py
+++ karaoke_bot/keyboards/keyboard_factory.py
@@ -6,18 +6,26 @@ class KeyboardFactory:
     def __init__(self, lm: LocalizationManager):
         self.lm = lm
 
-    def get_inline_keyboard(self, keyboard_name: str, lg_code='en'):
+    def get_inline_keyboard(self, keyboard_name: str, lg_code: str = 'en', callbacks_data: dict[int, str] | None = None):
         buttons_dict = self.lm.get_local_obj(obj_name=keyboard_name, keys=['buttons'])
 
         if buttons_dict is not None:
             keyboard = InlineKeyboardMarkup()
-            for button_dict in buttons_dict:
+            for idx, button_dict in enumerate(buttons_dict, start=1):
                 button_text = button_dict[lg_code]
-                callback_data = button_dict['callback_data']
+
+                if callbacks_data is None:
+                    callback_data = button_dict.get('callback_data')
+                else:
+                    callback_data = callbacks_data.get(idx)
+                    if callback_data is None:
+                        callback_data = button_dict.get('callback_data')
+
                 if button_dict['attach_mode'] == 'add':
                     keyboard.add(InlineKeyboardButton(text=button_text, callback_data=callback_data))
                 else:
                     keyboard.insert(InlineKeyboardButton(text=button_text, callback_data=callback_data))
+
             return keyboard
 
         raise KeyError(f"{keyboard_name} does not have keys=['buttons']")
