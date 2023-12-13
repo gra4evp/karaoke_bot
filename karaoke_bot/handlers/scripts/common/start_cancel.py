8987db9229c5ce60adcc4cce94fd357e3d74d303
gra4evp <petrgrachev1@gmail.com>
2023-12-13 19:15:33 +0300

    localize start_cancel.py

--- karaoke_bot/handlers/scripts/common/start_cancel.py
+++ karaoke_bot/handlers/scripts/common/start_cancel.py
@@ -1,16 +1,25 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
-from karaoke_bot.keyboards import other_keyboard
+from karaoke_bot.keyboards import keyboard_en
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from karaoke_bot.config import HELLO_TEXT, START_TEXT
 # from karaoke_bot.models.sqlalchemy_models_without_polymorph import TelegramProfile, Account, Session
 from karaoke_bot.models.sqlalchemy_data_utils import create_or_update_telegram_profile
 from karaoke_bot.handlers.scripts.visitor.search_karaoke import search_karaoke
 from karaoke_bot.handlers.scripts.common.register_telegram_user import register_telegram_user
+from karaoke_bot.localization.localization_manager import LocalizationManager
+from karaoke_bot.localization.local_files.scripts.common.loc_start_cancel import local_dict
+from karaoke_bot.keyboards.keyboard_factory import KeyboardFactory
+
+
+lm = LocalizationManager(local_dict=local_dict)
+kf = KeyboardFactory(lm=lm)
 
 
 async def start_command(message: types.Message, state: FSMContext):
+    fname = start_command.__name__
+    lg_code = message.from_user.language_code
 
     await register_telegram_user(message.from_user)
 
@@ -29,10 +38,18 @@ async def start_command(message: types.Message, state: FSMContext):
                 karaoke_name = parameters.get('karaoke_name')
                 if karaoke_name is not None:
                     message.text = karaoke_name
-                    await message.answer(HELLO_TEXT, reply_markup=other_keyboard, parse_mode='HTML')
+                    await message.answer(
+                        text=lm.localize_text(fname, lg_code, params=['messages', 'helo_text']),
+                        reply_markup=kf.get_reply_keyboard('keyboard_start', lg_code),
+                        parse_mode='HTML'
+                    )
                     await search_karaoke(message=message, state=state)
     else:
-        await message.answer(START_TEXT, reply_markup=other_keyboard, parse_mode='HTML')
+        await message.answer(
+            text=lm.localize_text(fname, lg_code, params=['messages', 'start_text']),
+            reply_markup=kf.get_reply_keyboard('keyboard_start', lg_code),
+            parse_mode='HTML'
+        )
 
 
 async def cancel_command(message: types.Message, state: FSMContext):
@@ -52,7 +69,7 @@ async def callback_cancel_command(callback: types.CallbackQuery, state: FSMConte
 def register_handlers(dispatcher: Dispatcher):
 
     dispatcher.register_callback_query_handler(callback_cancel_command, Text(equals='cancel'))
-    dispatcher.register_message_handler(cancel_command, Text(equals='cancel', ignore_case=True), state='*')
+    dispatcher.register_message_handler(cancel_command, Text(equals=["cancel", "отмена"], ignore_case=True), state='*')
     dispatcher.register_message_handler(cancel_command, commands=['cancel'], state='*')
 
     dispatcher.register_message_handler(start_command, commands=['start', 'help'], state='*')
