293d246ed93dc6544c7ebb108e8e745ff4dfe3b1
gra4evp <petrgrachev1@gmail.com>
2023-12-14 00:54:48 +0300

    refactoring localization_manager.py

--- karaoke_bot/localization/localization_manager.py
+++ karaoke_bot/localization/localization_manager.py
@@ -3,19 +3,9 @@ class LocalizationManager:
         self.local_dict = local_dict
         self.default_lg_code = default_lg_code
 
-    # TODO переписать код без метода localize_text в new_karaoke.py, order_track.py. Использовать новые методы снизу
     def localize_text(self, obj_name: str, lg_code: str, params: list) -> str:
-        loc_dict = self.local_dict.get(obj_name)
-        if loc_dict is not None:
-            for key in params:
-                if key not in loc_dict:
-                    raise KeyError(f'{obj_name}: Key "{key}" is not in {loc_dict}')
-
-                loc_dict = loc_dict[key]
-
-            return loc_dict.get(lg_code, loc_dict[self.default_lg_code])
-
-        raise KeyError(f'Key "{obj_name}" is not in local_text')
+        loc_dict = self.get_local_obj(obj_name, params)
+        return loc_dict.get(lg_code, loc_dict[self.default_lg_code])
 
     def get_local_obj(self, obj_name: str, keys: list | None = None) -> dict:
         obj_dict = self.local_dict.get(obj_name)
@@ -34,6 +24,4 @@ class LocalizationManager:
             obj_dict = obj_dict[key]
         return obj_dict
 
-    def get_local_value(self, obj_name: str, lg_code: str, params: list) -> str:
-        loc_dict = self.get_local_obj(obj_name, params)
-        return loc_dict.get(lg_code, loc_dict[self.default_lg_code])
+
