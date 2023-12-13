2b9a6da4f01862845f7e89c1d72dc7b1cc735166
gra4evp <petrgrachev1@gmail.com>
2023-12-13 19:13:58 +0300

    add method get_reply_keyboard

--- karaoke_bot/keyboards/keyboard_factory.py
+++ karaoke_bot/keyboards/keyboard_factory.py
@@ -1,10 +1,27 @@
 from karaoke_bot.localization.localization_manager import LocalizationManager
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
 
 class KeyboardFactory:
     def __init__(self, lm: LocalizationManager):
         self.lm = lm
+
+    def get_reply_keyboard(self, keyboard_name: str, lg_code: str = 'en'):
+        buttons_dict = self.lm.get_local_obj(obj_name=keyboard_name, keys=['buttons'])
+
+        if buttons_dict is not None:
+            keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+            for button_dict in buttons_dict:
+                button_text = button_dict[lg_code]
+
+                if button_dict['attach_mode'] == 'add':
+                    keyboard.add(KeyboardButton(button_text))
+                else:
+                    keyboard.insert(KeyboardButton(button_text))
+
+            return keyboard
+
+        raise KeyError(f"{keyboard_name} does not have keys=['buttons']")
 
     def get_inline_keyboard(self, keyboard_name: str, lg_code: str = 'en', callbacks_data: dict[int, str] | None = None):
         buttons_dict = self.lm.get_local_obj(obj_name=keyboard_name, keys=['buttons'])
